0f4847b2cd9c6f8a61eb2b0733cfe2a4c06012f6
pabloAfk <arturpablomm@gmail.com>
2025-09-22 19:11:04 -0300

    touchpad /etc/x11/xorg.conf.d/40-libinput.conf

---
+++ i3/touchpad etc x11 xorg.conf.d
@@ -0,0 +1,8 @@
+Section "InputClass"
+Identifier "touchpad catchall"
+MatchIsTouchpad "on"
+Driver "libinput"
+Option "Tapping" "on"
+Option "NaturalScrolling" "off"
+Option "AccelProfile" "flat"
+EndSection
