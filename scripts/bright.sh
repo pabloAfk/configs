018e8bfaaf53c85dc0570bbcd4de95f2a27a2bdf
pabloAfk <arturpablomm@gmail.com>
2026-01-05 23:35:03 -0300

    Add brightness adjustment script using dialog
    This script allows users to adjust screen brightness using brightnessctl with a dialog interface.

---
+++ scripts/bright.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+export DIALOGRC="$HOME/.dialogrc"
+
+# precisa instalar o brightnessctl
+cat > "$DIALOGRC" << 'EOF'
+screen_color = (WHITE,BLACK,ON)
+shadow_color = (BLACK,BLACK,ON)
+dialog_color = (WHITE,BLACK,ON)
+title_color = (WHITE,BLACK,ON)
+border_color = (WHITE,BLACK,ON)
+button_active_color = (BLACK,WHITE,ON)
+button_inactive_color = (WHITE,BLACK,ON)
+button_key_active_color = (BLACK,WHITE,ON)
+button_key_inactive_color = (WHITE,BLACK,ON)
+EOF
+
+LAST="1"  # começa em "aumentar"
+
+while true; do
+  BRIGHT=$(brightnessctl g)
+  MAX=$(brightnessctl m)
+  PERC=$((BRIGHT * 100 / MAX))
+
+  CHOICE=$(dialog \
+    --clear \
+    --default-item "$LAST" \
+    --menu "brilho: $PERC%" 12 40 4 \
+    1 "aumentar" \
+    2 "diminuir" \
+    3 "sair" \
+    3>&1 1>&2 2>&3)
+
+  clear
+
+  case "$CHOICE" in
+    1)
+      brightnessctl set +5%
+      LAST="1"
+      ;;
+    2)
+      brightnessctl set 5%-
+      LAST="2"
+      ;;
+    3|*)
+      exit 0
+      ;;
+  esac
+done
